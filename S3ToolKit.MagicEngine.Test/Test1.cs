98ff4f764031b24345f01d9c16f2d3d4343cdab3
Grant Hess <granthes@the-prof.net>
2012-07-13 19:34:51 -0600

    Initial Visual Studio and unit test framework

---
+++ S3ToolKit.MagicEngine.Test/Test1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace S3ToolKit.MagicEngine.Test
+{
+    [TestFixture]
+    public class Test1
+    {
+    }
+}
