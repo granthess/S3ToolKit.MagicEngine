60fa354c4676ebcca3ad852eac148fbdd3d66da6
Grant Hess <granthes@the-prof.net>
2012-07-13 22:20:36 -0600

    Updating for feature file change

---
+++ S3ToolKit.MagicEngine.BDD/PackageManagementSteps.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace S3ToolKit.MagicEngine.BDD
+{
+    [Binding]
+    public class PackageManagementSteps
+    {
+       
+    }
+}
